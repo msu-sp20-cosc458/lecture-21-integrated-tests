d03303c8dded815ab4e7f351ea8eda8f37c6ce5f
Phil B <philanab@gmail.com>
2020-03-08 18:05:15 -0400

    Update app.py

--- 20_flask/app.py
+++ 20_flask/app.py
@@ -1,1 +1,14 @@
+import os, flask
 
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+if __name__ == '__main__': # do not forget this line
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
