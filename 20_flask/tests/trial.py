914434bb5edf56112384d9bff4e568325dbe9a12
Phil B <philanab@gmail.com>
2020-03-08 18:05:57 -0400

    Update trial.py

--- 20_flask/tests/trial.py
+++ 20_flask/tests/trial.py
@@ -1,1 +1,14 @@
+import app, unittest, flask_testing, requests
 
+class ServerIntegrationTestCase(
+    flask_testing.LiveServerTestCase
+):
+    def create_app(self):
+        return app.app
+
+    def test_server_sends_hello(self):
+        r = requests.get(self.get_server_url())
+        self.assertEquals(r.text, 'Hello, world!')
+
+if __name__ == '__main__':
+    unittest.main()
