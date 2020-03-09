5ce6811740b61b6ff46107819ff3db22468e7a74
Phil B <philanab@gmail.com>
2020-03-08 23:18:44 -0400

    Update tests_socketio.py

--- 21_socketio/tests/tests_socketio.py
+++ 21_socketio/tests/tests_socketio.py
@@ -7,12 +7,12 @@ class SocketIOTestCase(unittest.TestCase):
 
         self.assertEquals(len(response), 1)
         from_server = response[0]
-        self.assertEquals(
+        self.assertEqual(
             from_server['name'], 
             "hello to client"
         )
         data = from_server['args'][0]
-        self.assertEquals(data['message'], "Hey there!")
+        self.assertEqual(data['message'], "Hey there!")
         
     def test_server_relays_message(self):
         client = app.socketio.test_client(app.app)
@@ -20,14 +20,14 @@ class SocketIOTestCase(unittest.TestCase):
             'my message': 'Purple is beautiful!'
         })
         response = client.get_received()
-        self.assertEquals(len(response), 2)
+        self.assertEqual(len(response), 2)
         from_server = response[1]
-        self.assertEquals(
+        self.assertEqual(
             from_server['name'],
             'got your message'
         )
         data = from_server['args'][0]
-        self.assertEquals(
+        self.assertEqual(
             data['your message'],
             u'Purple is beautiful!'
         )
