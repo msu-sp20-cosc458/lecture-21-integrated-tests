e8b7333772c3f00c87071fb4f978d385857583ba
Phil B <philanab@gmail.com>
2020-03-08 23:17:38 -0400

    Update tests_flask.py

--- 21_flask/tests/tests_flask.py
+++ 21_flask/tests/tests_flask.py
@@ -8,7 +8,7 @@ class ServerIntegrationTestCase(
 
     def test_server_sends_hello(self):
         response = requests.get(self.get_server_url())
-        self.assertEquals(response.text, "Something goes here instead!")
+        self.assertEqual(response.text, "Something goes here instead!")
 
 if __name__ == '__main__':
     unittest.main()
